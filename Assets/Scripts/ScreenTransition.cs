6d0c693ffa3a63205cb9e929463e3afa9b3dae6c
tigertora7571 <tigertora7571@gmail.com>
2024-01-19 13:27:47 +0900

    change meishi

--- Assets/Scripts/ScreenTransition.cs
+++ Assets/Scripts/ScreenTransition.cs
@@ -2,14 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
- 
-public class ScreenTransition : MonoBehaviour {
- 
-    
- 
+
+public class ScreenTransition : MonoBehaviour
+{
     //追加した
     //ボタンを押した時の処理
-    public void OnClick()
+    public void change_scene_button()
     {
         SceneManager.LoadScene("MeishiScene");
     }
